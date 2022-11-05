30ca880bb6ced293216f04721ac1c1f077cd849c
Magosogold <113687741+Magosogold@users.noreply.github.com>
2022-11-05 20:37:12 +0800

    游戏开始界面ver1.01
    修改了在loading界面点击后直接进入开始界面的bug（但是修的很诡异）

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
-
+    public GameObject backGround;
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0)&& backGround.transform.position.y>-700)
         {
-            GetComponent<Animator>().SetTrigger("SkipToGameStart");
+            GetComponent<Animator>().SetTrigger("IsSkip");
         }
+
+
     }
 
 
