5f2dbd16c97fc5fa8abefe9280792b6b17d4c6e0
Magosogold <113687741+Magosogold@users.noreply.github.com>
2022-11-06 14:50:59 +0800

    淡入淡出效果
    在UIManager里面

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
@@ -9,6 +10,12 @@ public class UIManager : MonoBehaviour
     public GameObject PauseUI;//暂停UI
     public GameObject ZArrowUI;//Z箭头UI本体
     public GameObject ZArrowUIParent;//Z箭头UI生成指向对象
+
+
+    //淡入淡出
+    public Image fadePlane;
+
+
     private void Awake()
     {
         if (instance == null)
@@ -43,4 +50,21 @@ public class UIManager : MonoBehaviour
     {
         ZArrowUI.SetActive(false);
     }
+
+
+    /*淡入淡出，调用方法：
+     *例： StartCoroutine(FadeCo(Color.clear, Color.black, 1));    
+     */
+    public IEnumerator FadeCo(Color from, Color to, float time)
+    {
+        float speed = 1 / time;
+        float percent = 0;
+        while (percent < 1)
+        {
+            percent += Time.deltaTime * speed;
+            fadePlane.color = Color.Lerp(from, to, percent);
+            yield return null;
+        }
+    }
+
 }
