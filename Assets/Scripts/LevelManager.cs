18fded68d0bdbbf7b418af519629d1811381cfbb
ZdxFly <1256425684@QQ.COM>
2022-11-07 01:03:31 +0800

    LevelManager1.0
    切换场景的方法没想好，先提交一下，后面改

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager instance;
+    public GameObject[] Scenes;//场景
+    public Player GamePlayer;
+    private UIManager UIManager;
+    [SerializeField]
+    private float time = 3f;//复活玩家等待时间
+    private int Index = 0;//计算场景更新
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            if (instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+    void Start()
+    {
+        Instantiate(Scenes[0]);//载入第一个场景
+    }
+    public void Respawn()
+    {
+        StartCoroutine("RespawnPlayer");//复活玩家
+    }
+
+    public IEnumerator RespawnPlayer()
+    {
+        GamePlayer.gameObject.SetActive(false);//玩家消失
+        yield return new WaitForSeconds(time);//等待时间
+        //GamePlayer.transform.position = GamePlayer.RespawnPosition;//设置重生点，Player里面还没写
+        GamePlayer.gameObject.SetActive(true);//玩家复活
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("ReactTag"))//可交互物体
+        {
+            UIManager.ZArrowAppear(collision.gameObject);//Z剪头UI出现在物体上
+            if (Input.GetKeyDown(KeyCode.Z))//按Z键交互
+            {
+                Debug.Log("交互");
+            }
+        }
+        if (collision.CompareTag("NextScene"))//换场景，方法还没想好
+        {
+            if (Index < 2)//数字是场景数量
+            {
+                Scenes[Index].SetActive(false);
+                Instantiate(Scenes[Index + 1]);
+                Respawn();//复活玩家
+                Index += 1;
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("ReactTag"))
+        {
+            UIManager.ZArrowDisappear();//Z剪头UI消失
+        }
+    }
+}
