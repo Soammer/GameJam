35921733968ad67962e3a2e22bfb102c31b639e4
woshiyuanfangdeni <117359194+woshiyuanfangdeni@users.noreply.github.com>
2022-11-11 11:06:42 +0800

    cameraFollowAndLimit

--- Assets/Scripts/cameraFollow.cs
+++ Assets/Scripts/cameraFollow.cs
@@ -6,9 +6,13 @@ public class cameraFollow : MonoBehaviour
 {
     private bool followerX;//x方向视角移动
     private bool followerY;//y方向视角移动
+    private float cameraPosX;
+    private float cameraPosY;
 
     public Transform playerPos;//玩家位置
-    public float parameter = 0.1f;
+    public float parameter = 0.01f;
+    public Vector2 minPosition;
+    public Vector2 maxPosition;
 
     public static cameraFollow instance;
     public void Awake()
@@ -18,21 +22,31 @@ public class cameraFollow : MonoBehaviour
 
     private void LateUpdate()
     {
-        if(playerPos != null)
+        cameraPosX = transform.position.x;
+        cameraPosY = transform.position.y;
+        if (playerPos != null)
         {
-            if(transform.position != playerPos.position)
+            if (transform.position != playerPos.position)
             {
+                Vector3 targetPos = playerPos.position;
                 if (followerX)
                 {
-                    Vector3 targetPos = new Vector3(playerPos.position.x, 1, 1);
-                    transform.position = Vector3.Lerp(transform.position,targetPos,parameter);
+                    targetPos.y = cameraPosY;
+                    targetPos.x = Mathf.Clamp(targetPos.x, minPosition.x, maxPosition.x);
+                    transform.position = Vector3.Lerp(transform.position, targetPos, parameter);
                 }else if (followerY)
                 {
-                    Vector3 targetPos = new Vector3(1, playerPos.position.y, 1);
+                    targetPos.x = cameraPosX;
+                    targetPos.y = Mathf.Clamp(targetPos.y, minPosition.y, maxPosition.y);
                     transform.position = Vector3.Lerp(transform.position, targetPos, parameter);
                 }
+                
             }
         }
     }
-    
+    public void cameraLimited(Vector2 minPos,Vector2 maxPos)
+    {
+        minPosition = minPos;
+        maxPosition = maxPos;
+    }
 }
