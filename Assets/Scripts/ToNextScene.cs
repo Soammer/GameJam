a63c18f5bec4e1dbab5a280123105cefb2303474
ZdxFly <1256425684@QQ.COM>
2022-11-12 00:18:12 +0800

    第三关基本场景搭建
    部分交互功能还未实现，对ToNextScene有细小修改，云移动的脚本还没写

--- Assets/Scripts/ToNextScene.cs
+++ Assets/Scripts/ToNextScene.cs
@@ -5,10 +5,7 @@ using UnityEngine;
 public class ToNextScene : MonoBehaviour
 {
     private Collider2D Coll;
-    public bool isSwitch;//判断同时变场景角色的关卡
     public GameObject[] Scenes;//变换关卡
-    public GameObject[] SwitchScenes;//同时变关卡角色的场景
-    //private bool isChange = true;//改变场景
 
     // Start is called before the first frame update
     private void Start()
@@ -17,7 +14,7 @@ public class ToNextScene : MonoBehaviour
     }
     private void Update()
     {
-        SwitchScene();
+         
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -35,21 +32,5 @@ public class ToNextScene : MonoBehaviour
         Instantiate(Scenes[1]);
     }
 
-    void SwitchScene()
-    {
-        /*if (isSwitch && Input.GetKeyDown(KeyCode.)//可以按键改变场景
-        {
-            isChange = !isChange;
-            if (isChange)
-            {
-                SwitchScenes[0].SetActive(true);
-                SwitchScenes[1].SetActive(false);
-            }
-            else
-            {
-                SwitchScenes[0].SetActive(false);
-                SwitchScenes[1].SetActive(true);
-            }
-        }*/
-    }
+
 }
