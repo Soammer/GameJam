4b4772a5bdd922f062fb24c683172c786660c4a9
LepusRattan <117378808+LepusRattan@users.noreply.github.com>
2022-11-12 19:05:47 +0800

    心都碎了
    ESC那个，做暂停界面的重新写一下？

--- Assets/Scripts/Slash.cs
+++ Assets/Scripts/Slash.cs
@@ -11,12 +11,19 @@ public class Slash : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "enemy")
+        {
+            other.GetComponent<Enemy>().ChangeHealth(-damage);
+        }
     }
 }
