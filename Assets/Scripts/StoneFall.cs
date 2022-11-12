517c9c86a503f3066c0e2c106808054592e06bbc
希儿单推人 <hh75624777163@163.com>
2022-11-13 00:44:47 +0800

    石头掉落

--- Assets/Scripts/StoneFall.cs
+++ Assets/Scripts/StoneFall.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class StoneFall : MonoBehaviour
 {
-    private bool isFall=false,isAttack=false;
-    private float fallSpeed;
+    private bool isFall=false;
+    private float fallSpeed=1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +15,8 @@ public class StoneFall : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(gameObject.transform.position.x>=0)
+        if(gameObject.transform.position.y>=-7)
         {
-            if(isAttack)
-            {
-
-            }
             if(isFall)
             {
                 fall();
@@ -31,20 +27,23 @@ public class StoneFall : MonoBehaviour
 
         }
     }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag=="Player")
+        {
+            //½ÇÉ«ËÀÍö
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("1");
         if(other.tag=="Player")
         {
             isFall = true;
-            isAttack = true;
         }
     }
     private void fall()
     {
-        gameObject.transform.position += new Vector3(0, 0, 0);
-    }
-    private void attack()
-    {
-
+        gameObject.transform.position -= new Vector3(0, fallSpeed, 0);
     }
 }
