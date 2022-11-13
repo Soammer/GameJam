307c446103d2e2c3b95213b0fc3fdca34dcf7700
流明的直肠好温暖 <113687741+Magosogold@users.noreply.github.com>
2022-11-13 11:16:00 +0800

    开始界面
    没有退出
    修改了游戏画质静美的bug

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
     public GameObject backGround;
+    public Image imagePlane;
     private void Update()
     {
         if (Input.GetMouseButtonDown(0)&& backGround.transform.position.y>-700)
@@ -15,7 +18,30 @@ public class MenuManager : MonoBehaviour
 
     }
 
+    public void GameStartButton()
+    {
+        StartCoroutine(MenuFadeCo(Color.clear, Color.black, 3, 3));//完全没有用
+        StartCoroutine(StartCo(3));
+    }
 
+    //偷过来淡入淡出，但没用
+    private IEnumerator MenuFadeCo(Color from, Color to, float time, float sleepTime)
+    {
+        yield return new WaitForSeconds(sleepTime);
+        float speed = 1 / time;
+        float percent = 0;
+        while (percent < 1)
+        {
+            percent += Time.deltaTime * speed;
+            imagePlane.color = Color.Lerp(from, to, percent);
+            yield return null;
+        }
+    }
 
-
+    //也没用
+    private IEnumerator StartCo(float sleepTime)
+    {
+        yield return new WaitForSeconds(sleepTime);
+        SceneManager.LoadScene("Game");
+    }
 }
